677a2bc96f0d3b1c83c2af7ee57d678ebeb69af9
nargesmn100 <nargesmn004@gmail.com>
2023-09-18 13:52:43 -0400

    bug fixed

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,10 +1,10 @@
 import java.util.List;
 
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+    // TASK 1: fix this code so that it passes the test in DataTypesTest.java. Done!
     public static long sum(List<Integer> numbers) {
 
-        int s = 0; // type 'int' can only store so many numbers so it  gives us the incorrect number.
+        long s = 0; // type 'int' can only store so many numbers so it  gives us the incorrect number.
 
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
